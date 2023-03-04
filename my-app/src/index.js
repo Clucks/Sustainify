fb673168b0da10fd5d52a60bfe84461818025e3d
Vincent <vincent854321@gmail.com>
2023-03-04 16:35:49 -0600

    i dont remember

--- my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Leaderboard from './App';
-import './index.css';
-
-ReactDOM.render(<Leaderboard />, document.getElementById('root'));
---
+++ script.js
@@ -0,0 +1,64 @@
+/*Note that I assumed that you wanted to render the leaderboard to the document body, so I used document.createElement to create a new div and appendChild to append the returned element of the Leaderboard function to the body. If you want to render it to a specific element, you can replace document.body with the element's selector.*/
+class User {
+    constructor(name, score) {
+      this.name = name;
+      this.score = score;
+    }
+  }
+  
+  const users = [
+    {
+      name: "user1",
+      score: Math.floor(Math.random() * 101)
+    },
+    {
+      name: "You",
+      score: Math.floor(Math.random() * 101)
+    },
+    {
+      name: "user3",
+      score: Math.floor(Math.random() * 101)
+    },
+    {
+      name: "user4",
+      score: Math.floor(Math.random() * 101)
+    }
+  ];
+  
+  const sortedUsers = users.sort((u1, u2) =>
+    u1.score < u2.score ? 1 : u1.score > u2.score ? -1 : 0
+  );
+  
+  function Leaderboard() {
+    const [usrs, updateUsers] = useState(sortedUsers);
+  
+    function renderUsers() {
+      return usrs.map((user, index) => (
+        '<div>' +
+          '<p>' +
+            (index +
+              1 +
+              ')              ' +
+              user.name +
+              '                  ' +
+              user.score) +
+          '</p>' +
+        '</div>'
+      )).join('');
+    }
+  
+    useEffect(() => {
+      renderUsers();
+    });
+  
+    const leaderboardDiv = document.createElement('div');
+    leaderboardDiv.innerHTML = `
+      <h1 class="title"> Leaderboard </h1>
+      <h2 class="subheader"> Username Score</h2>
+      <h3>${renderUsers()}</h3>
+    `;
+  
+    return leaderboardDiv;
+  }
+  
+  document.body.appendChild(Leaderboard());
